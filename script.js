02db9e418e969491f5454a04d352ae675bcf2315
Jason Leong <soundslikejason@users.noreply.github.com>
2017-04-23 10:59:43 -0700

    variable declarations with let and const

---
+++ script.js
@@ -0,0 +1,85 @@
+// Lecture: let and const
+
+
+// ES5
+var name5 = 'Jane Smith';
+var age = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+// // ES6 const variables creates an immutable binding and will produce error
+// const name6 = 'Jane Smith';
+// let age6 = 23;
+// name6 = 'Jane Miller';
+// console.log(name6);
+
+
+// ES5 variables are function scoped
+function driversLicense5(passedTest) {
+  // console.log(firstName);
+  if (passedTest) {
+    var firstName = 'John';
+    var yearOfBirth = 1990;
+  }
+  
+  console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+  
+}
+
+driversLicense5(true);
+
+
+// ES6 variables are blocked scoped. Can only use variable after declaring and defining.
+function driversLicense6(passedTest) {
+  // console.log(firstName)
+  let firstName = 'John';
+  const yearOfBirth = 1990;
+  
+  if (passedTest) {
+    firstName = 'John';
+  }
+  
+  console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+
+driversLicense6(true);
+
+
+// // ES5 - value of i, due to function scoped
+// var i = 23;
+// for (var i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+// console.log(i);
+
+// ES6 - value of i, due to block scoped
+let i = 23;
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+console.log(i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
