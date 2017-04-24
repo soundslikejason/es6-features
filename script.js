5d462baa252c4ffe89e9d86427569ea72aae509d
Jason Leong <soundslikejason@users.noreply.github.com>
2017-04-24 09:21:48 -0700

    strings

--- script.js
+++ script.js
@@ -84,6 +84,28 @@ for (let j = 0; j < 5; j++) {
 
 
 
+///////////////////////////////////////////////
+// Lecture: Strings
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+  return 2016 - year;
+}
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
+
+// ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('J'));
+console.log(n.endsWith('th'));
+console.log(n.includes('oh'));
+console.log(`${firstName} `.repeat(5));
 
 
 
