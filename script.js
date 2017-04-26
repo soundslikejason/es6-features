0da844e308a85dc901f83876505ebf97f8c0ef73
Jason Leong <soundslikejason@users.noreply.github.com>
2017-04-26 09:57:03 -0700

    default parameters

--- script.js
+++ script.js
@@ -384,26 +384,78 @@
 
 
 
-// ES5
-function isFullAge5(limit) {
-  var argsArr = Array.prototype.slice.call(arguments, 1);
-  
-  argsArr.forEach(function(cur) {
-    console.log((2016 - cur) >= limit);
-  });
-}
+// // ES5
+// function isFullAge5(limit) {
+//   var argsArr = Array.prototype.slice.call(arguments, 1);
+//
+//   argsArr.forEach(function(cur) {
+//     console.log((2016 - cur) >= limit);
+//   });
+// }
+//
+// // isFullAge5(16, 1990, 1999, 1965);
+// // isFullAge5(1990, 1999, 1965, 2016, 1987);
+//
+//
+// // ES6
+// // used in function declaration to receive arbitrary number of parameters
+// function isFullAge6(limit, ...years) {
+//   years.forEach(cur => console.log((2016 - cur) >= limit));
+// }
+//
+// isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
 
-// isFullAge5(16, 1990, 1999, 1965);
-// isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+
+///////////////////////////////////////////////
+// Lecture: Default parameters
+
+// // ES5
+// function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+//
+//   lastName === undefined ? lastName = 'Smith' : lastName = lastName;
+//   nationality === undefined ? nationality = 'american' : nationality = nationality;
+//
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.yearOfBirth = yearOfBirth;
+//   this.nationality = nationality;
+// }
 
 
 // ES6
-// used in function declaration to receive arbitrary number of parameters
-function isFullAge6(limit, ...years) {
-  years.forEach(cur => console.log((2016 - cur) >= limit));
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
+
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.yearOfBirth = yearOfBirth;
+  this.nationality = nationality;
 }
 
-isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
+var john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
+
+
+// // if default parameter is not at the end of arguments
+// function SmithPerson(firstName, lastName = 'Smith', yearOfBirth, nationality = 'american') {
+//
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.yearOfBirth = yearOfBirth;
+//   this.nationality = nationality;
+// }
+//
+// var john = new SmithPerson('John', undefined, 1990);
+// var emily = new SmithPerson('Emily', 'Diaz', 1983, 'spanish');
+
+
+
+
+
+
+
+
+
 
 
 
