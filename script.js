6a6e0e37fc6235e0ec02c5f89bf918b30b73ffba
Jason Leong <soundslikejason@users.noreply.github.com>
2017-04-26 09:14:44 -0700

    rest parameters

--- script.js
+++ script.js
@@ -324,36 +324,86 @@
 ////////////////////////////////////////////////
 // Lecture: Spread operator
 
-function addFourAges(a, b, c, d) {
-  return a + b + c + d;
-};
+// function addFourAges(a, b, c, d) {
+//   return a + b + c + d;
+// };
+//
+// var sum1 = addFourAges(18, 30, 12, 21);
+//
+// console.log(sum1);
+//
+// // ES5
+// var ages = [18, 30, 12, 21];
+// var sum2 = addFourAges.apply(null, ages);
+// console.log(sum2);
+//
+// // ES6
+// // expand elements of an array or node list (iterable object)
+// const sum3 = addFourAges(...ages);
+// console.log(sum3);
+//
+//
+// const familySmith = ['John', 'Jane', 'Mark'];
+// const familyMiller = ['Mary', 'Bob', 'Ann'];
+// const bigFamily = [...familySmith, 'Lily', ...familyMiller];
+// console.log(bigFamily);
+//
+// const h = document.querySelector('h1');
+// const boxes = document.querySelectorAll('.box');
+// const all = [h, ...boxes];
+//
+// Array.from(all).forEach(cur => cur.style.color = 'purple');
+// // all.forEach(cur => cur.style.color = 'purple');
 
-var sum1 = addFourAges(18, 30, 12, 21);
 
-console.log(sum1);
+
+//////////////////////////////////////////////////////
+// Lecture: Rest parameters
+
+// // ES5
+// function isFullAge5() {
+//   // console.log(arguments);
+//   var argsArr = Array.prototype.slice.call(arguments);
+//
+//   argsArr.forEach(function(cur) {
+//     console.log((2016 - cur) >= 18);
+//   });
+// }
+//
+// isFullAge5(1990, 1999, 1965);
+// isFullAge5(1990, 1999, 1965, 2016, 1987);
+//
+//
+// // ES6
+// // used in function declaration to receive arbitrary number of parameters
+// function isFullAge6(...years) {
+//   years.forEach(cur => console.log((2016 - cur) >= 18));
+// }
+//
+// isFullAge6(1990, 1999, 1965, 2016, 1987);
+
+
 
 // ES5
-var ages = [18, 30, 12, 21];
-var sum2 = addFourAges.apply(null, ages);
-console.log(sum2);
+function isFullAge5(limit) {
+  var argsArr = Array.prototype.slice.call(arguments, 1);
+  
+  argsArr.forEach(function(cur) {
+    console.log((2016 - cur) >= limit);
+  });
+}
+
+// isFullAge5(16, 1990, 1999, 1965);
+// isFullAge5(1990, 1999, 1965, 2016, 1987);
+
 
 // ES6
-// expand elements of an array or node list (iterable object)
-const sum3 = addFourAges(...ages);
-console.log(sum3);
+// used in function declaration to receive arbitrary number of parameters
+function isFullAge6(limit, ...years) {
+  years.forEach(cur => console.log((2016 - cur) >= limit));
+}
 
-
-const familySmith = ['John', 'Jane', 'Mark'];
-const familyMiller = ['Mary', 'Bob', 'Ann'];
-const bigFamily = [...familySmith, 'Lily', ...familyMiller];
-console.log(bigFamily);
-
-const h = document.querySelector('h1');
-const boxes = document.querySelectorAll('.box');
-const all = [h, ...boxes];
-
-Array.from(all).forEach(cur => cur.style.color = 'purple');
-// all.forEach(cur => cur.style.color = 'purple');
+isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
 
 
 
