3157bbaa0deeaabaaabd73dbe6fedd38f0b3fb29
Jason Leong <soundslikejason@users.noreply.github.com>
2017-04-27 10:07:18 -0700

    classes

--- script.js
+++ script.js
@@ -453,45 +453,82 @@
 ////////////////////////////////////////////
 // Lecture: Maps
 
-const question = new Map();
-question.set('question', 'What is the official name of the latest major JavaScript version?');
-question.set(1, 'ES5');
-question.set(2, 'ES6');
-question.set(3, 'ES2015');
-question.set(4, 'ES7');
-question.set('correct', 3);
-question.set(true, 'Correct answer');
-question.set(false, "Wrong, please try again");
+// const question = new Map();
+// question.set('question', 'What is the official name of the latest major JavaScript version?');
+// question.set(1, 'ES5');
+// question.set(2, 'ES6');
+// question.set(3, 'ES2015');
+// question.set(4, 'ES7');
+// question.set('correct', 3);
+// question.set(true, 'Correct answer');
+// question.set(false, "Wrong, please try again");
+//
+// console.log(question.get('question'));
+// // console.log(question.size);
+//
+// if (question.has(4)) {
+//   // question.delete(4);
+//   // console.log('Answer 4 is here');
+// }
+//
+// // question.clear();
+//
+// // question.forEach((value, key) =>
+// // console.log(`This is ${key}, and it's set to ${value}`));
+//
+//
+// for (let [key, value] of question.entries()) {
+//   if (typeof(key) === 'number') {
+//     console.log(`Answer ${key}: ${value}`);
+//   }
+// }
+//
+// const ans = parseInt(prompt('Write the correct answer'));
+// console.log(question.get(ans === question.get('correct')));
 
-console.log(question.get('question'));
-// console.log(question.size);
 
-if (question.has(4)) {
-  // question.delete(4);
-  // console.log('Answer 4 is here');
+
+//////////////////////////////////////////////////
+// Lecture: Classes
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
 }
 
-// question.clear();
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+}
 
-// question.forEach((value, key) =>
-// console.log(`This is ${key}, and it's set to ${value}`));
+var john5 = new Person5('John', 1990, 'teacher');
 
-
-for (let [key, value] of question.entries()) {
-  if (typeof(key) === 'number') {
-    console.log(`Answer ${key}: ${value}`);
+// ES6
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+  
+  calculateAge() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+  
+  static greeting() {
+    console.log('Hey there!');
   }
 }
 
-const ans = parseInt(prompt('Write the correct answer'));
-console.log(question.get(ans === question.get('correct')));
+const john6 = new Person6('John', 1990, 'teacher');
 
+Person6.greeting();
 
-
-
-
-
-
+// Note: class definitions are not hoisted, unlike function constructors.
+// Can only add methods to classes, not properties.
 
 
 
